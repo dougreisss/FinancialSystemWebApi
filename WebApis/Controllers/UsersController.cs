9f7540f006f0f5a837cf9d1ca7c6f2d0ac773035
dougreisss <douglas_rrs@hotmail.com>
2023-07-11 21:55:58 -0300

    add token

---
+++ WebApis/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApis.Models;
+using WebApis.Token;
+
+namespace WebApis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [AllowAnonymous]
+        [Produces("application/json")]
+        [HttpPost("CriarToken")]
+        public async Task<IActionResult> CriarTokenIdentity([FromBody] Login login)
+        {
+            if (string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.password))
+            {
+                return Unauthorized();
+            }
+
+            var result = _signInManager.PasswordSignInAsync(login.email, login.password, false, lockoutOnFailure: false);
+
+            if (result.IsCompleted)
+            {
+                // Recupera Usuário Logado
+                var userCurrent = await _userManager.FindByEmailAsync(login.email);
+                var idUsuario = userCurrent.Id;
+
+                var token = new TokenJWTBuilder()
+                    .AddSecurityKey(JwtSecurityKey.Create("Secret_Key-12345678"))
+                .AddSubject("Empresa - Canal Dev Net Core")
+                .AddIssuer("Teste.Securiry.Bearer")
+                .AddAudience("Teste.Securiry.Bearer")
+                .AddClaim("idUsuario", idUsuario)
+                .AddExpiry(5)
+                .Builder();
+
+                return Ok(token.value);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
